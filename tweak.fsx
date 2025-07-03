a97e5d5576c35f5eaf94be524a865d37d3042d47
Ben Hutchison <ben@aldaviva.com>
2025-07-03 10:45:53 -0700

    Don't add Unicode BOM to UTF-8 files again. apktool fails to recompile resources again, so skip and just reuse original precompiled resources.

--- tweak.fsx
+++ tweak.fsx
@@ -4,15 +4,16 @@ open System.Text.RegularExpressions
 
 let decompiledDir = "./decompiled"
 let changedComment = "# changed by Ben"
+let utf8 = new UTF8Encoding(false, true)
 
 let file1 = Seq.head (Directory.EnumerateFiles(decompiledDir, "WeatherHomeActivity.smali", SearchOption.AllDirectories))
-let file1Contents = File.ReadAllText( file1, Encoding.UTF8)
+let file1Contents = File.ReadAllText( file1, utf8)
 let file1ContentsModified = Regex.Replace (file1Contents, 
     @"(?<=const-wide/16 (?<timeValueRegister>[vp]\d+), )0x3(?=\s*invoke-static \{\1(?:, [vp]\d+){3}\}, Lio\/reactivex\/Completable;->timer\(JLjava\/util\/concurrent\/TimeUnit;Lio\/reactivex\/Scheduler;\)Lio\/reactivex\/Completable;)", 
     $"0x0 {changedComment}", RegexOptions.Singleline)
 
 let file2 = Seq.head(Directory.EnumerateFiles(decompiledDir, "SplashScreenAppLaunchController$WaitForCoreComponentsState.smali", SearchOption.AllDirectories))
-let file2Contents = File.ReadAllText(file2, Encoding.UTF8)
+let file2Contents = File.ReadAllText(file2, utf8)
 let file2ContentsModified = Regex.Replace(file2Contents,
     @"(?<=const-wide/16 (?<timeValueRegister>[pv]\d+), )0x7d0\b(?=.{1,300}invoke-static \{\1(?:, [pv]\d+){2}\}, Lio\/reactivex\/Completable;->timer\(JLjava\/util\/concurrent\/TimeUnit;\)Lio\/reactivex\/Completable;)",
     $"0x0 {changedComment}", RegexOptions.Singleline)
@@ -24,5 +25,5 @@ elif file2Contents = file2ContentsModified then
     printfn "No match in %s" file2
     exit 1
 else 
-    File.WriteAllText(file1, file1ContentsModified, Encoding.UTF8)
-    File.WriteAllText(file2, file2ContentsModified, Encoding.UTF8)
+    File.WriteAllText(file1, file1ContentsModified, utf8)
+    File.WriteAllText(file2, file2ContentsModified, utf8)
